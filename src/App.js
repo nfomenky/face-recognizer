1505db16f64e5e19099a9edb1ebf7a234efbed14
Nkongchou Fomenky <nfomenky@student.ysu.edu>
2020-04-07 22:21:13 -0500

    added face recognition functionality with clarify API

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 
 const particlesOptions = {
   particles: {
@@ -18,7 +20,38 @@ const particlesOptions = {
   }
 }
 
+const app = new Clarifai.App({
+  apiKey: '164b79f82b9f4f5fa8584dc47e32d62f'
+});
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+    app.models.predict(
+      "a403429f2ddf4b49b307e318f00e528b",
+      "https://samples.clarifai.com/face-det.jpg")
+      .then(
+        function (response) {
+          // do something with response
+          console.log(response);
+        },
+        function (err) {
+          // there was an error
+        }
+      );
+
+  }
   render() {
     return (
       <div className='App'>
@@ -28,8 +61,8 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/*<FaceRecognition />*/}
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        <FaceRecognition />
       </div>
     )
   }
