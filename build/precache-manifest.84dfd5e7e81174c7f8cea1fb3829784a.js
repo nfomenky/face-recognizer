4ad90ef3a815b22a0271207f3a701410d0e597fa
Nkongchou Fomenky <nfomenky@student.ysu.edu>
2020-05-04 08:35:46 -0500

    deployed on gh pages

---
+++ build/precache-manifest.84dfd5e7e81174c7f8cea1fb3829784a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "487216e20d46787fa301340e3b1b9a00",
+    "url": "/face-recognizer/index.html"
+  },
+  {
+    "revision": "e8d643a7f3f8e9dc3164",
+    "url": "/face-recognizer/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "66938023e00e31f7fff2",
+    "url": "/face-recognizer/static/css/main.34807ab1.chunk.css"
+  },
+  {
+    "revision": "e8d643a7f3f8e9dc3164",
+    "url": "/face-recognizer/static/js/2.945ed11b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-recognizer/static/js/2.945ed11b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66938023e00e31f7fff2",
+    "url": "/face-recognizer/static/js/main.53053126.chunk.js"
+  },
+  {
+    "revision": "de133aa0e151db7765e2",
+    "url": "/face-recognizer/static/js/runtime-main.eb46276d.js"
+  }
+]);
